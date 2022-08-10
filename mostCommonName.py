f4b20322ee25d1bd4c5b8bc6f8605747f9881c52
Scott Lee <scottlee@Pro-BookMac-10.local>
2022-08-10 20:48:27 +0900

    <first git push>

---
+++ mostCommonName.py
@@ -0,0 +1,12 @@
+def mostCommonName(L):
+    dictionary1 = {}
+    max = 0
+    s = set()
+    for element in L:
+        count = dictionary1.get(element, 0) + 1
+        dictionary1[element] = count
+    if dictionary1[element] > max:
+        max = count
+    if dictionary1[element] == max:
+        s.add(element)
+    return s
